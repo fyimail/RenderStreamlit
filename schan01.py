43bdf6475cf1841326941cbcb85b51bb39332f28
FM Young <fyimail@gmail.com>
2023-07-18 19:59:43 +0600

    remove dotenv

--- schan01.py
+++ schan01.py
@@ -1,11 +1,8 @@
 import os
-from dotenv import load_dotenv
 from langchain.llms import OpenAI
 from langchain.agents import load_tools, initialize_agent, AgentType
 from langchain.callbacks import StreamlitCallbackHandler
 import streamlit as st
-
-load_dotenv()
 
 llm = OpenAI(temperature=0, streaming=True, openai_api_key=os.getenv('OPENAI_API_KEY'))
 tools = load_tools(["ddg-search"])
